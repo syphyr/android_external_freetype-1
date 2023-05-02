9204df96d0de306b624e88ddc72f9a72a928e115
Seigo Nonaka <nona@google.com>
2023-05-02 10:01:38 +0900

    Cherrypick following three changes
    [cherrypick 545bf3a27] [sfnt, truetype] Add `size_reset` to `MetricsVariations`.
    [cherrypick daad10810] [truetype] tt_size_reset_height to take FT_Size
    [cherrypick 51ad7b243] [services] FT_Size_Reset_Func to return FT_Error
    
    Bug: 278221085
    Test: TreeHugger
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:9fe9411db4b7e715a39c0ccf48d1e0328f1d8e7c)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:8d63b0bfcbaba361543fd9394b8d86907f52c97d)
    Merged-In: I7e839b2a36e35c27974a82cc76e853996a7c7688
    Change-Id: I7e839b2a36e35c27974a82cc76e853996a7c7688

--- src/cff/cffdrivr.c
+++ src/cff/cffdrivr.c
@@ -940,7 +940,8 @@
                        FT_UInt   gindex,
                        FT_Int   *avalue )
   {
-    FT_Service_MetricsVariations  var = (FT_Service_MetricsVariations)face->var;
+    FT_Service_MetricsVariations
+      var = (FT_Service_MetricsVariations)face->tt_var;
 
 
     return var->hadvance_adjust( FT_FACE( face ), gindex, avalue );
@@ -950,7 +951,8 @@
   static void
   cff_metrics_adjust( CFF_Face  face )
   {
-    FT_Service_MetricsVariations  var = (FT_Service_MetricsVariations)face->var;
+    FT_Service_MetricsVariations
+      var = (FT_Service_MetricsVariations)face->tt_var;
 
 
     var->metrics_adjust( FT_FACE( face ) );
@@ -969,7 +971,8 @@
     (FT_BSB_Adjust_Func)     NULL,                   /* bsb_adjust      */
     (FT_VOrg_Adjust_Func)    NULL,                   /* vorg_adjust     */
 
-    (FT_Metrics_Adjust_Func) cff_metrics_adjust      /* metrics_adjust  */
+    (FT_Metrics_Adjust_Func) cff_metrics_adjust,     /* metrics_adjust  */
+    (FT_Size_Reset_Func)     NULL                    /* size_reset      */
   )
 #endif
 
