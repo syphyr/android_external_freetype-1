ac3fc93cac783a45f3c12cc7b024e422dacf928f
Seigo Nonaka <nona@google.com>
2025-03-06 14:57:56 +0900

    DO NOT MERGE - Return if the subglyph count is too large
    Bug: 399065987
    Test: Manually done
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:cc531278152b19d67133ba1ef1116752a35a9e6e)
    Merged-In: I82c28208ecb4260cbcb6709447ae362e26c00ead
    Change-Id: I82c28208ecb4260cbcb6709447ae362e26c00ead

--- src/truetype/ttgload.c
+++ src/truetype/ttgload.c
@@ -1931,6 +1931,11 @@
 
         /* construct an outline structure for              */
         /* communication with `TT_Vary_Apply_Glyph_Deltas' */
+        if ( gloader->current.num_subglyphs > (SHRT_MAX - 4) ) {
+            error = FT_Err_Invalid_Argument;
+            goto Exit;
+        }
+
         outline.n_points   = (short)( gloader->current.num_subglyphs + 4 );
         outline.n_contours = outline.n_points;
 
